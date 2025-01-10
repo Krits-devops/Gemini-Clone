8e42b533721ccdebb75034425c7dc9ca42820b9d
Krits-devops <kritagyarm9@gmail.com>
2025-01-09 19:57:09 -0500

    Restructuring components and Gemini API & Modules Added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html', // For Vite
+    './src/**/*.{js,jsx,ts,tsx}', // For React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
